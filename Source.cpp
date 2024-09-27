0963602ebe07b71ae5f867aeae925d59c76e9435
User <User@SSD2100>
2024-09-27 20:55:32 +0300

    Клас зроблено

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 #include"Organizer.h"
+#include <cstdlib>;
+#include <windows.h>;
 using namespace std;
 
 int main()
 {
-	Organizer organizer("data.tx");
-	//organizer.EnterJobs(2);
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Organizer organizer("data.txå");
+	//organizer.EnterJobs(3);
 	organizer.LoadFromFile();
 	organizer.PrintJobs();
 	//organizer.SaveToFile();
